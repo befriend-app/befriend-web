1da05ceaa4c2bc0086d4456e157cc065cc8dfc87
egekhter <egekhter@gmail.com>
2025-03-17 10:44:06 -0500

    features navigation, styles

--- routes/web.js
+++ routes/web.js
@@ -2,6 +2,7 @@ let express = require('express');
 let router = express.Router();
 
 let webController = require('../controllers/web');
+const ejs = require("ejs");
 
 router.get('/', function (req, res, next) {
     return new Promise(async (resolve, reject) => {
@@ -62,5 +63,37 @@ router.get('/waitlist/confirm', function (req, res, next) {
         resolve();
     });
 });
+
+router.get('/email/unsubscribe/:user_code', function (req, res, next) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            await webController.getUnsubscribe(req, res);
+        } catch (e) {
+            return res.json("Error loading page", 400);
+        }
+
+        resolve();
+    });
+});
+
+router.get('/test', function (req, res, next) {
+    return new Promise(async (resolve, reject) => {
+        try {
+            let view_path = joinPaths(getRepoRoot(), `tmp/email/views/preview.ejs`);
+
+            let html = await ejs.renderFile(view_path, {
+                user_code: 123,
+            });
+
+            res.send(html);
+        } catch (e) {
+            console.error(e);
+        }
+
+        resolve();
+    });
+});
+
+
 
 module.exports = router;
